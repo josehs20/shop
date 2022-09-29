d4758fcbda5c5513f61583dfbcda1e61216b507d
josehs20 <josehenriquesimiquelsouza@gmail.com>
2022-09-29 08:31:07 -0300

    quantidade em pedido itens

--- app/Console/Commands/CriaPedidoTeste.php
+++ app/Console/Commands/CriaPedidoTeste.php
@@ -42,19 +42,19 @@ class CriaPedidoTeste extends Command
     public function handle()
     {
         $ptcs = ProdTamCor::take(5)->orderByRaw("RAND()")->get();
-        $user = User::with(['enderecos','pedidos'])->where('perfil', 'cliente')->first();
-        
+        $user = User::with(['enderecos', 'pedidos'])->where('perfil', 'cliente')->first();
+
         $pedido = $user->pedidos()->create([
             'valor_total' => 0,
             'status' => 'in',
             'data' => now('America/Sao_Paulo')->format('Y-m-d H:i:m'),
             'endereco_id' => $user->enderecos[0]->id
         ]);
-    
-      foreach ($ptcs as $key => $v) {
-              $pedido->pedido_itens()->create(['ptc_id' => $v->id]);
+
+        foreach ($ptcs as $key => $v) {
+            $pedido->pedido_itens()->create(['ptc_id' => $v->id, 'quantidade' => rand(1, 5)]);
         }
-       $itens = PedidoItem::with('ptc')->where('pedido_id', $pedido->id)->get();
+        $itens = PedidoItem::with('ptc')->where('pedido_id', $pedido->id)->get();
         echo $itens;
     }
 }
