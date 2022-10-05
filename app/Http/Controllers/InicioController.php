5a080d3382ff9cf960c720f2a9627d271f9a20e0
jonatasmcampos <camposjonatas3@gmail.com>
2022-10-05 09:55:56 -0300

    retornando preco maior e menor de cada produto

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use App\Models\Imagem;
+use App\Models\ProdTamCor;
 use App\Models\Produto;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
 
 class InicioController extends Controller
 {
     public function index(){
         $categorias = Categoria::all();
-        $produtos = Produto::all();
+        $produtos = Produto::with('prodTamCors')->get()->map(function($value){
+            $maior = $value->prodTamCors()->orderBy('preco', 'desc')->first()->preco;
+            $menor = $value->prodTamCors()->orderBy('preco', 'asc')->first()->preco;
+            $mm = ['precoMaior' => $maior, 'precoMenor' => $menor, 'nome' => $value->nome];
+            return $mm;
+        });
+
+        // dd($produtos);
         return view('usuario.welcome', compact('categorias', 'produtos'));
     }
 }
--- resources/views/usuario/produto/variosProdutos.blade.php
+++ resources/views/usuario/produto/variosProdutos.blade.php
@@ -26,15 +26,20 @@
 </style>
 
 <section class="varios-produtos">
-    @foreach ($produtos as $produto)
+    @foreach ($produtos as $produto)    
             <div class="card me-2 mb-3 produto" style="width: 15rem;">
                 <div class="ver-produto">Ver produto</div>
                 <img src="https://img.elo7.com.br/product/main/2B34062/blusa-de-frio-moletom-personalizada-nike-tamanho-m-blusa-de-frio-moletom-personalizada-nike.jpg"
                     class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $produto->nome }}</h5>
+                    <h5 class="card-title">{{ $produto['nome'] }}</h5>
                     <p class="card-text">
-                        
+                        @if ($produto['precoMenor'] == $produto['precoMaior'])
+                            {{$produto['precoMenor']}}
+                        @else
+                            {{$produto['precoMenor']}} - 
+                            {{$produto['precoMaior']}}
+                        @endif
                     </p>
                 </div>
             </div>
